f817af4be13d644e585d4772b5df737132d8c66a
lucaspokrywa <lucas.pokrywa@gmail.com>
2026-07-28 15:58:41 +0200

    Correction du test de benchmark sur les machines

--- benchmark/docker-entrypoint.sh
+++ benchmark/docker-entrypoint.sh
@@ -5,5 +5,5 @@ exec wrk \
     "-t${BENCHMARK_THREADS:-16}" \
     "-c${BENCHMARK_CONNECTIONS:-1000}" \
     "-d${BENCHMARK_DURATION:-30s}" \
-    -s /example.lua \
+    -s /scripts/example.lua \
     "${BENCHMARK_URL:-http://haproxy/}"
